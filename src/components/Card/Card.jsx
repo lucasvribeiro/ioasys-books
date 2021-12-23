d4f0e4e0ec21a66778ddf6f9e7e54abe107ad1ee
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-12-23 19:28:47 -0300

    Adjustments in Card Component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,12 +7,18 @@ const StyledCard = styled.div`
   height: 160px;
   border-radius: 4px;
   box-shadow: 0px 6px 24px rgba(84, 16, 95, 0.13);
-
   padding: 22px 20px;
+  overflow-y: hidden;
+
+  transition: all 0.3s ease;
+
+  &:hover {
+    box-shadow: 0px 6px 24px rgba(84, 16, 95, 0.2);
+  }
 `;
 
-const Card = ({ children }) => {
-  return <StyledCard>{children}</StyledCard>;
+const Card = ({ children, className }) => {
+  return <StyledCard className={className}>{children}</StyledCard>;
 };
 
 export default Card;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
+import Card from "../../components/Card/Card";
+
 import "./Home.css";
 
 const Home = () => {
-  return <div>home page</div>;
+  return (
+    <div className="home-page">
+      <Card className="book-card">
+        <div className="book-cover-container">capa</div>
+        <div className="book-info-container">
+          <div className="book-top-info">
+            <h3>Book title</h3>
+            <p>Author</p>
+          </div>
+
+          <div className="book-bottom-info">
+            <p>pages</p>
+            <p>editora</p>
+            <p>published</p>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
