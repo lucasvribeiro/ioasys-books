07ed6dca78d7354d61ccfed88d35f5410b5cdfd5
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-12-23 19:20:26 -0300

    Created Card Component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
-const Card = () => {
-  return <div>card</div>;
+const StyledCard = styled.div`
+  background-color: #ffffff;
+  width: 272px;
+  height: 160px;
+  border-radius: 4px;
+  box-shadow: 0px 6px 24px rgba(84, 16, 95, 0.13);
+
+  padding: 22px 20px;
+`;
+
+const Card = ({ children }) => {
+  return <StyledCard>{children}</StyledCard>;
 };
 
 export default Card;
